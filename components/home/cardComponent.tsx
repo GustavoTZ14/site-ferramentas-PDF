b12f46a81932fe126852a4dc3c3c0985c0b82a53
GustavoTZ14 <gterrin155@gmail.com>
2026-09-17 17:06:39 -0300

    up

--- components/home/cardComponent.tsx
+++ components/home/cardComponent.tsx
@@ -5,7 +5,7 @@ export default function Card() {
   return (
     <>
       {dadosCard.map(({ id, name, imagem, descricao }) => (
-        <div key={id} className="grid w-full h-50 outline-1 outline-gray-300 shadow-xl bg-[#f4f4f4] p-4">
+        <div key={id} className="grid h-50 outline-1 outline-gray-300 shadow-xl bg-[#f4f4f4] p-4">
           <div className="relative w-20 h-20">
             <Image src={imagem} alt="autotag" fill className="object-cover" />
           </div>
