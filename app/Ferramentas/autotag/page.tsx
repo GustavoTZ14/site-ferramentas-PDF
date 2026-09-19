d919594d3a6396ce895f3af427f61c77f6ed37c6
GustavoTZ14 <gterrin155@gmail.com>
2026-09-19 12:45:20 -0300

    upload

---
+++ app/Ferramentas/autotag/page.tsx
@@ -0,0 +1,9 @@
+import UploadComponent from "@/components/home/UploadComponent";
+
+export default function Autotag() {
+  return (
+    <main className="w-full min-h-screen">
+      <UploadComponent />
+    </main>
+  )
+}
--- components/home/UploadComponent.tsx
+++ components/home/UploadComponent.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 import { useState, useRef } from "react";
+import { Trash } from 'lucide-react';
 
 export default function Hero() {
   const [arquivo, setArquivo] = useState<File[]>([]);
@@ -14,24 +15,74 @@ export default function Hero() {
     }
   }
 
+  function excluirArquivo(index: number) {
+    setArquivo((filesAtuais) =>
+      filesAtuais.filter((_, i) => i !== index)
+    );
+  }
+
   return (
     <>
       <section className="p-15">
         <article className="grid justify-center items-center text-center gap-5">
-          <div className="grid grid-cols-2 outline-1 outline-gray-500 p-2 gap-2">
-            <output className="grid grid-cols-2 col-span-2 text-start outline-1 outline-gray-500 p-2 h-40 overflow-auto">
-              {arquivo.map(({ name, size }) => (
-                <div key={name}>
-                  <h1>{name}</h1>
-                  <span>{(size / 1024 / 1024).toFixed(2)} MB</span>
-                </div>
-              ))}
-            </output>
-            <input type="file" multiple accept=".pdf,.docx,.doc,.xlsx,.pptx" className="flex col-span-1 p-2 outline-1 outline-gray-500 h-10" onChange={(e) => {
+          <div>
+            <label htmlFor="file" className="bg-red-500 text-white p-2 cursor-pointer text-xl font-bold">
+              Selecionar arquivo
+            </label>
+            <input type="file" id="file" multiple accept=".pdf,.docx,.doc,.xlsx,.pptx" className="hidden" onChange={(e) => {
               const files = Array.from(e.target.files ?? []);
               setArquivo(files)
             }} />
-            <button className="col-span-1 outline-1 outline-gray-500 p-2" onClick={Limpar}>Limpar</button>
+          </div>
+          <div className="grid gap-2 w-250">
+            <div className="flex justify-between">
+              <button className="flex gap-2 items-center bg-gray-500 text-white text-sm p-2 cursor-pointer" title="Limpar tudo" onClick={Limpar}>
+                Limpar tudo
+              </button>
+            </div>
+          </div>
+          <div className="w-full h-100 outline-1 outline-gray-300 overflow-auto bg-white/30">
+            <table className="w-full border-collapse">
+              <thead>
+                <tr className="bg-blue-100">
+                  <th className="border border-gray-300 p-2">Nome</th>
+                  <th className="border border-gray-300 p-2">Tipo</th>
+                  <th className="border border-gray-300 p-2">Tamanho</th>
+                  <th className="border border-gray-300 p-2">Excluir</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {arquivo.map((file, index) => (
+                  <tr
+                    key={index}
+                    className="odd:bg-white even:bg-blue-50"
+                  >
+                    <td className="border border-gray-300 p-2 text-start">
+                      {file.name}
+                    </td>
+
+                    <td className="border border-gray-300 p-2">
+                      {file.type}
+                    </td>
+
+                    <td className="border border-gray-300 p-2">
+                      {file.size}
+                    </td>
+
+                    <td className="border border-gray-300 p-2">
+                      <button
+                        onClick={() => excluirArquivo(index)}
+                        className="cursor-pointer text-red-600"
+                        title="excluir"
+                      >
+                        <Trash size={15} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </article>
       </section >
